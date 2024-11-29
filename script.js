1c8be8d65ab98fd0c14f485ad9d45fd59a4d3f52
toivo <toivopek@gmail.com>
2024-11-29 11:38:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function click1() {
+    document.body.style.background = "linear-gradient(to right, #ff8ff2, #aaffa6)";
+}
+function click2() {
+    document.body.style.background = "linear-gradient(to right, #fff, #aaffa6)";
+}
